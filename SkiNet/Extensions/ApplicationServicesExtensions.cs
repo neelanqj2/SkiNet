8bfa1f13cf12a189006986774e686bdec5d79324
neelanqj2 <neelan.q.joachimpillai@gmail.com>
2024-05-27 14:08:28 -0700

    End of Section 13

--- SkiNet/Extensions/ApplicationServicesExtensions.cs
+++ SkiNet/Extensions/ApplicationServicesExtensions.cs
@@ -3,6 +3,7 @@ using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SkiNet.Errors;
+using StackExchange.Redis;
 using System.Runtime.CompilerServices;
 
 namespace SkiNet.Extensions
@@ -23,6 +24,12 @@ namespace SkiNet.Extensions
                 //opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             } //, ServiceLifetime.Scoped
             );
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
+            });
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
